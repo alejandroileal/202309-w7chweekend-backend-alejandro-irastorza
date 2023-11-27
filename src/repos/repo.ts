2797adfe7ee8eda0c3e3b22740521630fc0da785
alejandroileal <aleal1202@outlook.com>
2023-11-27 21:18:43 +0100

    Add resting methods

--- src/repos/repo.ts
+++ src/repos/repo.ts
@@ -5,4 +5,8 @@ export interface Repository<X extends { id: unknown }> {
   create(_newItem: Omit<X, 'id'>): Promise<X>;
   update(_id: X['id'], _updatedItem: Partial<X>): Promise<X>;
   delete(_id: X['id']): Promise<void>;
+  addFriend(_friendId: X['id'], _currentUserId: X['id']): Promise<X>;
+  addEnemy(_enemyId: X['id'], _currentUserId: X['id']): Promise<X>;
+  removeFriend(_friendId: X['id'], _currentUserId: X['id']): Promise<X>;
+  removeEnemy(_enemyId: X['id'], _currentUserId: X['id']): Promise<X>;
 }
